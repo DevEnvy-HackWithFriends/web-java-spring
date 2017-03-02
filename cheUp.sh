3840107795070e400d806f6be13cdec19f5c245d
John Walsh <mjohnw@med.umich.edu>
2017-03-02 18:21:00 -0500

    [REF] name = CHE_HWF

--- cheUp.sh
+++ cheUp.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/data:/data -v $PWD/:/chedir eclipse/che:latest dir up
+docker run -it --rm --name CHE_HWF -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/data:/data -v $PWD/:/chedir eclipse/che:latest dir up
